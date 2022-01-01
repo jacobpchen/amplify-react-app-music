b6e71a8672829016df362e5d6919110e63b593ea
Jacob Chen <jacobpchen@gmail.com>
2022-01-01 15:28:59 -0500

    Added a paper display to display the song information

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import React, { useEffect, useState } from 'react'
 import Amplify, { API, graphqlOperation } from 'aws-amplify'
 import awsconfig from './aws-exports'
 import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react'
+
 import { listSongs } from './graphql/queries'
+import { updateSong } from './graphql/mutations'
+
+import { Paper, IconButton } from '@material-ui/core'
+import PlayArrowIcon from '@material-ui/icons/PlayArrow'
+import FavoriteIcon from '@material-ui/icons/Favorite'
 
 import './App.css';
 
@@ -27,12 +33,54 @@ function App() {
     }
   }
 
+  const addLike = async idx => {
+    try {
+      const song = songs[idx];
+      song.likes = song.likes + 1;
+      delete song.createdAt;
+      delete song.updatedAt;
+
+      const songData = await API.graphql(graphqlOperation(updateSong, { input: song }));
+      const songList = [...songs];
+      songList[idx] = songData.data.updateSong;
+      setSongs(songList);
+    } catch (error) {
+      console.log('error on adding Like to song', error);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <AmplifySignOut />
         <h2>My App Content</h2>
       </header>
+
+      <div className="songList">
+        {songs.map((song, idx) => {
+          return (
+            <Paper variant="outlined" elevation={2} key={`song${idx}`}>
+              <div className="songCard">
+                <IconButton aria-label="play">
+                  <PlayArrowIcon />
+                </IconButton>
+                <div>
+                  <div className="songTitle">{song.title}</div>
+                  <div className="songOwner">{song.owner}</div>
+                </div>
+                <div>
+                  <IconButton aria-label="like" onClick={() => addLike(idx)}>
+                    <FavoriteIcon />
+                  </IconButton>
+                  {song.likes}
+                </div>
+                <div className="songDescription">{song.description}</div>
+              </div>
+            </Paper>
+          );
+        })}
+      </div>
+
     </div>
   );
 }
