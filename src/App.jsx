43dcd3bea17b3595cadeb0756eda67751ca4c063
Jacob Chen <jacobpchen@gmail.com>
2022-01-01 11:32:09 -0500

    Added graphql auto generated files from aws

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+import Amplify, { API, graphqlOperation } from 'aws-amplify'
+import awsconfig from './aws-exports'
+import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react'
+import { listSongs } from './graphql/queries'
+
+import './App.css';
+
+Amplify.configure(awsconfig)
+
+function App() {
+
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    fetchSongs()
+  }, [])
+
+  const fetchSongs = async () => {
+    try {
+      const songData = await API.graphql(graphqlOperation(listSongs))
+      const songList = songData.data.listSongs.items
+      console.log('song list', songList)
+      setSongs(songList)
+    } catch (error) {
+      console.log('error on fetching songs', error)
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AmplifySignOut />
+        <h2>My App Content</h2>
+      </header>
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
